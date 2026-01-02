71e39791820c5714c833ffd71315baa558f1aafc
abhinav11 <abhinavpandey2405@gmail.com>
2026-01-02 13:39:36 +0530

    done with the doc page

--- src/Pages/Appointment.jsx
+++ src/Pages/Appointment.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, {useContext, useEffect, useState} from 'react'
+import {useParams} from "react-router";
+import {AppContext} from "../Context/Context.jsx";
 
 const Appointment = () => {
+
+  const {docId} = useParams();
+  const {doctors} = useContext(AppContext)
+  const [docinfo,setDocinfo] = useState(null)
+
+
+  const fetchDoInfo= async () =>{
+    const docInfo=doctors.find(doc=>doc._id==docId)
+    setDocinfo(docInfo);
+
+  }
+  useEffect(()=>{
+
+    fetchDoInfo()
+  },[doctors,docId])
   return (
-    <div>Appointment</div>
+    <div>
+      u
+
+
+
+    </div>
+
   )
 }
 
